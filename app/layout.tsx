75a75a37b28b0dd8b844954b46e60403f4afed59
eth3rnetfr4me <brinyipeti@mailbox.unideb.hu>
2025-07-09 23:52:46 +0200

    new

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,24 +1,41 @@
-'use client';
-
 import * as React from 'react';
 import { NextAppProvider } from '@toolpad/core/nextjs';
+import DashboardIcon from '@mui/icons-material/Dashboard';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 import { AppRouterCacheProvider } from '@mui/material-nextjs/v15-appRouter';
+import type { Navigation } from '@toolpad/core/AppProvider';
 import LinearProgress from '@mui/material/LinearProgress';
 import theme from '../theme';
-import { NAVIGATION } from './navigation';
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+const NAVIGATION: Navigation = [
+  {
+    kind: 'header',
+    title: 'Main items',
+  },
+  {
+    segment: '',
+    title: 'Introduction',
+    icon: <DashboardIcon />,
+  },
+  {
+    segment: 'page2',
+    title: 'Powerful Websites',
+    icon: <ShoppingCartIcon />,
+  },
+];
+
+export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="en" data-toolpad-color-scheme="light">
-    <body>
-    <AppRouterCacheProvider options={{ enableCssLayer: true }}>
-    <React.Suspense fallback={<LinearProgress />}>
-    <NextAppProvider theme={theme} navigation={NAVIGATION}>
-    {children}
-    </NextAppProvider>
-    </React.Suspense>
-    </AppRouterCacheProvider>
-    </body>
+      <body>
+        <AppRouterCacheProvider options={{ enableCssLayer: true }}>
+          <React.Suspense fallback={<LinearProgress />}>
+            <NextAppProvider theme={theme} navigation={NAVIGATION}>
+              {children}
+            </NextAppProvider>
+          </React.Suspense>
+        </AppRouterCacheProvider>
+      </body>
     </html>
   );
 }
